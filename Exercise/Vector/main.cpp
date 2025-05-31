56f3af62b085e2946c8166056e13ac6163166a77
Radoslaw Marcinowski <rm92.git@gmail.com>
2025-05-31 10:28:50 +0200

    Vector Exercise 2

--- Exercise/Vector/main.cpp
+++ Exercise/Vector/main.cpp
@@ -10,46 +10,36 @@ void PrintVec(const std::vector<int>& v) {
 }
 
 int main() {
-    // 1. Stworz wektor o wartosciach 1, 2, 3, 4, 5, 6
-    std::vector<int> vec = {1, 2, 3, 4, 5, 6};
-    std::cout << "1:\t";
+    // 1. Stworz pusty wektor
+    std::vector<int> vec;
     PrintVec(vec);
 
-    // 2. Usun pierwsza wartosc
-    auto it = std::remove(vec.begin(), vec.end(), vec[0]);
-    std::cout << "2:\t";
-    PrintVec(vec);
+    // 2. Wypisz rozmiar i pojemnosc wektora
+    std::cout << "2:\t"
+              << "Size: " << vec.size() << ", Capacity: " << vec.capacity() << std::endl;
 
-    // 3. Usuwanie smieci
-    vec.erase(it, vec.end());
+    // 3. Zmien rozmiar wektora na 10 i wypelnij go wartosciami 5
+    vec.reserve(10);
+    vec.insert(vec.begin(), vec.capacity(), 5);
     std::cout << "3:\t";
     PrintVec(vec);
 
-    // 4. Dodaj wartosc 5 na koncu wektora
-    vec.push_back(5);
-    std::cout << "4:\t";
-    PrintVec(vec);
+    // 4. Wypisz rozmiar i pojemnosc wektora
+    std::cout << "4:\t"
+              << "Size: " << vec.size() << ", Capacity: " << vec.capacity() << std::endl;
 
-    // 5. Dodaj wartosc 12 na poczatku wektora korzystajac z metody emplace()
-    vec.emplace(vec.begin(), 12);
-    std::cout << "5:\t";
-    PrintVec(vec);
+    // 5. Zarezerwuj pamiec na 20 elementow
+    vec.reserve(20);
 
-    // 6. Wypisz rozmiar wektora i maksymalny mozliwy rozmiar
+    // 6. Wypisz rozmiar i pojemnosc wektora
     std::cout << "6:\t"
               << "Size: " << vec.size() << ", Capacity: " << vec.capacity() << std::endl;
 
-    // 7. Wypisz zawartosc wektora
-    std::cout << "7:\t";
-    PrintVec(vec);
+    // 7. Zredukuj pojemnosc wektora metoda shrink_to_fit()
+    vec.shrink_to_fit();
 
-    // 8. Wyczysc wektor
-    vec.clear();
-    std::cout << "8:\t";
-    PrintVec(vec);
-
-    // 9. Wypisz rozmiar wektora
-    std::cout << "9:\t"
+    // 8. Wypisz rozmiar i pojemnosc wektora
+    std::cout << "8:\t"
               << "Size: " << vec.size() << ", Capacity: " << vec.capacity() << std::endl;
 
     return 1;
