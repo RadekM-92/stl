cf720da41a47d1b758c9280bec3cc3e6ddfc8d5b
Radoslaw Marcinowski <rm92.git@gmail.com>
2025-06-06 11:19:25 +0200

    Added lengthSort() implementation

---
+++ homework/length-sort/sort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+
+#include "sort.hpp"
+
+
+std::deque<std::string> lengthSort(const std::forward_list<std::string>& flist) {
+    std::deque<std::string> deq;
+
+    for (auto element : flist) {
+        deq.emplace_back(element);
+    }
+
+    std::sort(deq.begin(), deq.end(), [](std::string str1, std::string str2) {
+        if (str1.size() == str2.size()) {
+            auto it1 = str1.begin();
+            auto it2 = str2.begin();
+            for (; it1 != str1.end(); ++it1, ++it2) {
+                if (*it1 == *it2) {
+                    continue;
+                } else {
+                    if (*it1 < *it2) {
+                        return true;
+                    } else if (*it1 > *it2) {
+                        return false;
+                    }
+                }
+            }
+        } else {
+            if (str1.size() < str2.size()) {
+                return true;
+            } else if (str1.size() > str2.size()) {
+                return false;
+            }
+        }
+    });
+
+    return deq;
+}
